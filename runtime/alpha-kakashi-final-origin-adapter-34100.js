d4485639626d4edf57ab065d99209c064d88f4ed
NinjaSteveXer0 <sc68957hcm@privaterelay.appleid.com>
2026-09-18 22:56:08 +0700

    chore: activate Kakashi generation 44 browser RED fix

--- runtime/alpha-kakashi-final-origin-adapter-34100.js
+++ runtime/alpha-kakashi-final-origin-adapter-34100.js
@@ -69,7 +69,8 @@ const W2C_NONKILL_PATH="runtime/alpha-kakashi-w2c-nonkill-35780.js";
 // const BUILD="kakashi-final-20260918-40";
 // const BUILD="kakashi-final-20260918-41";
 // const BUILD="kakashi-final-20260918-42";
-const BUILD="kakashi-final-20260918-43";
+// const BUILD="kakashi-final-20260918-43";
+const BUILD="kakashi-final-20260918-44";
 
 function builtin(name){
   if(typeof process!=="undefined"&&process&&typeof process.getBuiltinModule==="function")return process.getBuiltinModule(name);
