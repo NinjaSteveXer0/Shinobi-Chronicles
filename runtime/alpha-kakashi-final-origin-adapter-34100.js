836251ee1b850ac39b758b759730a3ae56141bf3
NinjaSteveXer0 <sc68957hcm@privaterelay.appleid.com>
2026-09-19 21:22:39 +0700

    Kakashi: bump delivery generation to 60

--- runtime/alpha-kakashi-final-origin-adapter-34100.js
+++ runtime/alpha-kakashi-final-origin-adapter-34100.js
@@ -86,7 +86,7 @@ const POST_MI_PURSUIT_PATH="runtime/alpha-kakashi-post-mi-death-pursuit-35830.js
 // const BUILD="kakashi-final-20260919-54";
 // const BUILD="kakashi-final-20260919-57";
 // const BUILD="kakashi-final-20260919-58";
-const BUILD="kakashi-final-20260919-59";
+const BUILD="kakashi-final-20260919-60";
 
 function builtin(name){
   if(typeof process!=="undefined"&&process&&typeof process.getBuiltinModule==="function")return process.getBuiltinModule(name);
