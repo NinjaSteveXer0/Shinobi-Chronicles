5c80a4a3765af8ce22105fd2f45de56ddae19e2e
NinjaSteveXer0 <sc68957hcm@privaterelay.appleid.com>
2026-09-19 22:34:56 +0700

    Fix generation 64 loader newline

--- runtime/alpha-kakashi-final-origin-adapter-34100.js
+++ runtime/alpha-kakashi-final-origin-adapter-34100.js
@@ -89,7 +89,8 @@ const POST_MI_PURSUIT_PATH="runtime/alpha-kakashi-post-mi-death-pursuit-35830.js
 // const BUILD="kakashi-final-20260919-60";
 // const BUILD="kakashi-final-20260919-61";
 // const BUILD="kakashi-final-20260919-62";
-// const BUILD="kakashi-final-20260919-63";\nconst BUILD="kakashi-final-20260919-64";
+// const BUILD="kakashi-final-20260919-63";
+const BUILD="kakashi-final-20260919-64";
 
 function builtin(name){
   if(typeof process!=="undefined"&&process&&typeof process.getBuiltinModule==="function")return process.getBuiltinModule(name);
