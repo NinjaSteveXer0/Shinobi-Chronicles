49591b8f11c155bafe3c53f8e0ef55694d67d469
NinjaSteveXer0 <sc68957hcm@privaterelay.appleid.com>
2026-09-20 17:18:56 +0700

    Kakashi gen83: bump terminal reward guard delivery

--- runtime/alpha-kakashi-final-origin-adapter-34100.js
+++ runtime/alpha-kakashi-final-origin-adapter-34100.js
@@ -113,7 +113,8 @@ const BROWSER_ACCEPTANCE_PATH="runtime/alpha-kakashi-browser-acceptance-35950.js
 // const BUILD="kakashi-final-20260920-79";
 // const BUILD="kakashi-final-20260920-80";
 // const BUILD="kakashi-final-20260920-81";
-const BUILD="kakashi-final-20260920-82";
+// const BUILD="kakashi-final-20260920-82";
+const BUILD="kakashi-final-20260920-83";
 
 function builtin(name){
   if(typeof process!=="undefined"&&process&&typeof process.getBuiltinModule==="function")return process.getBuiltinModule(name);
