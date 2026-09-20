a47804243b39ba0dee6c2ef4f4ded6accae20c41
NinjaSteveXer0 <sc68957hcm@privaterelay.appleid.com>
2026-09-20 18:39:46 +0700

    Kakashi gen86: bump final adapter delivery

--- runtime/alpha-kakashi-final-origin-adapter-34100.js
+++ runtime/alpha-kakashi-final-origin-adapter-34100.js
@@ -116,7 +116,8 @@ const BROWSER_ACCEPTANCE_PATH="runtime/alpha-kakashi-browser-acceptance-35950.js
 // const BUILD="kakashi-final-20260920-82";
 // const BUILD="kakashi-final-20260920-83";
 // const BUILD="kakashi-final-20260920-84";
-const BUILD="kakashi-final-20260920-85";
+// const BUILD="kakashi-final-20260920-85";
+const BUILD="kakashi-final-20260920-86";
 
 function builtin(name){
   if(typeof process!=="undefined"&&process&&typeof process.getBuiltinModule==="function")return process.getBuiltinModule(name);
