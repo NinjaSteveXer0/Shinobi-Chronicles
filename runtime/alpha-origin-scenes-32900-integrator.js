d9b2a965bb13d583265d0e711a4e6fa25c92e7e8
NinjaSteveXer0 <sc68957hcm@privaterelay.appleid.com>
2026-09-20 18:22:10 +0700

    Kakashi gen85: advance Story decision delivery

--- runtime/alpha-origin-scenes-32900-integrator.js
+++ runtime/alpha-origin-scenes-32900-integrator.js
@@ -99,7 +99,8 @@ globalThis.runAlphaOriginScene32900Diagnostics=runAlphaOriginScene32900Diagnosti
   // runtime/alpha-story-decision-realisation-34000.js?sc=story-decision-20260920-10
   // runtime/alpha-story-decision-realisation-34000.js?sc=story-decision-20260920-11
   // runtime/alpha-story-decision-realisation-34000.js?sc=story-decision-20260920-12
-  script.src="runtime/alpha-story-decision-realisation-34000.js?sc=story-decision-20260920-13";
+  // runtime/alpha-story-decision-realisation-34000.js?sc=story-decision-20260920-13
+  script.src="runtime/alpha-story-decision-realisation-34000.js?sc=story-decision-20260920-14";
   document.head.appendChild(script);
 })();
 })();
