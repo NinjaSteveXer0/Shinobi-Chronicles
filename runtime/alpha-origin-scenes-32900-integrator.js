3ce4106e6ccac2f82ee4a49912c46f075babe130
NinjaSteveXer0 <sc68957hcm@privaterelay.appleid.com>
2026-09-22 01:42:08 +0700

    ci: advance Kakashi repair-to-Golden delivery generation

--- runtime/alpha-origin-scenes-32900-integrator.js
+++ runtime/alpha-origin-scenes-32900-integrator.js
@@ -103,7 +103,7 @@ globalThis.runAlphaOriginScene32900Diagnostics=runAlphaOriginScene32900Diagnosti
   // runtime/alpha-story-decision-realisation-34000.js?sc=story-decision-20260920-14
   // runtime/alpha-story-decision-realisation-34000.js?sc=story-decision-20260920-15
   // runtime/alpha-story-decision-realisation-34000.js?sc=story-decision-20260921-16
-  script.src="runtime/alpha-story-decision-realisation-34000.js?sc=story-decision-20260922-22";
+  script.src="runtime/alpha-story-decision-realisation-34000.js?sc=story-decision-20260922-23";
   document.head.appendChild(script);
 })();
 })();
