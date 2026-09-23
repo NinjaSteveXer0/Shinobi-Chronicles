4cf8ab5eef470e22e4ef358ccdca3841b7d8c179
NinjaSteveXer0 <sc68957hcm@privaterelay.appleid.com>
2026-09-24 00:00:58 +0700

    Add #334 transition ownership to Alpha closure gate

--- tools/qa_issue_141_pre_alpha_runtime_closure.py
+++ tools/qa_issue_141_pre_alpha_runtime_closure.py
@@ -74,6 +74,7 @@ NODE_GATES = [
     "tools/qa_browser_runtime_error_gate_311.js",
     "tools/qa_release_candidate_evidence_311.js",
     "tools/qa_issue_312_story_battle_benchmark.js",
+    "tools/qa_issue_334_story_transition_owner.js",
 ]
 
 EXTERNAL_ASSET_GATES = [
